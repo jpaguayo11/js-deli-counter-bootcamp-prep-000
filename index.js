5dcc679d42c9c9bece01bd6614349663381efc92
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-06 23:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ function takeANumber (katzDeliLine, name) {
 }
 
 function nowServing (katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine[0]}.`
-    return katzDeliLine.shift()
+  if (katzDeliLine.length > 0) {.
+    var x = `Currently serving ${katzDeliLine[0]}.`
+    katzDeliLine.shift()
+    return x
+
   } else {
     return "There is nobody waiting to be served!"
   }
